7693861dd661b7d918b0d3e929decdde37bfc246
Ritankar Bhattacharjee <ritankar.bhattacharjee80@gmail.com>
2026-03-30 21:50:14 +0530

    implement fonts

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -7,9 +7,8 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function Index() {
   return (
     <SafeAreaView className="flex-1 justify-center items-center bg-background">
-      <Text className="text-5xl text-success">
-        This is a nativewind powered app
-      </Text>
+      <Text className="text-5xl text-success font-bold">Home</Text>
+      <Text className="text-5xl text-success font-sans-extrabold">Home</Text>
     </SafeAreaView>
   );
 }
