f217b9acb26fb6c39bb62127f6d8b633a539941e
Ritankar Bhattacharjee <ritankar.bhattacharjee80@gmail.com>
2026-03-30 20:07:19 +0530

    tabs designed and working

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,20 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import "../global.css";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(auth)" />
-      <Stack.Screen name="(tabs)" />
-      <Stack.Screen name="subscription" />
-    </Stack>
+    <>
+      <StatusBar style="dark" />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="(auth)" />
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="subscriptions/[id]" />
+      </Stack>
+    </>
   );
 }
